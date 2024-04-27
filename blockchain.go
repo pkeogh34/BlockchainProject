51aa85519b904367fd678d08d53d02ddc52e879f
pkeogh34 <pkeogh34@gmail.com>
2024-04-27 10:50:01 +0100

    Add cli

--- blockchain.go
+++ blockchain.go
@@ -1,7 +1,20 @@
 package BlockchainProject
 
+import (
+	"github.com/boltdb/bolt"
+)
+
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
+
 type Blockchain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
+}
+
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
 }
 
 func (bc *Blockchain) AddBlock(data string) {
@@ -13,13 +26,22 @@ func (bc *Blockchain) AddBlock(data string) {
 
 		return nil
 	})
+	if err != nil {
+		panic(err)
+	}
 
 	newBlock := NewBlock(data, lastHash)
 
 	err = bc.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
 		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			return err
+		}
 		err = b.Put([]byte("l"), newBlock.Hash)
+		if err != nil {
+			return err
+		}
 		bc.tip = newBlock.Hash
 
 		return nil
@@ -29,6 +51,9 @@ func (bc *Blockchain) AddBlock(data string) {
 func NewBlockchain() *Blockchain {
 	var tip []byte
 	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		panic(err)
+	}
 
 	err = db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
@@ -36,8 +61,17 @@ func NewBlockchain() *Blockchain {
 		if b == nil {
 			genesis := NewGenesisBlock()
 			b, err := tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				return err
+			}
 			err = b.Put(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				return err
+			}
 			err = b.Put([]byte("l"), genesis.Hash)
+			if err != nil {
+				return err
+			}
 			tip = genesis.Hash
 		} else {
 			tip = b.Get([]byte("l"))
@@ -49,4 +83,29 @@ func NewBlockchain() *Blockchain {
 	bc := Blockchain{tip, db}
 
 	return &bc
+}
+
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+
+	return bci
+}
+
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	i.currentHash = block.PrevBlockHash
+
+	return block
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module BlockchainProject
+
+go 1.22.2
+
+require github.com/boltdb/bolt v1.3.1
+
+require golang.org/x/sys v0.19.0 // indirect
